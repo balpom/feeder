9cb0ffab1516ea4e0b4c8c705274231f19b10dd1
Mikhail <141855513+balpom@users.noreply.github.com>
2025-08-16 06:54:13 +0300

    Add files via upload

---
+++ src/Creators/AbstractCreator.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Balpom\Feeder\Creators;
+
+use Balpom\Feeder\CreatorInterface;
+use Balpom\Entity\Structures\AbstractStructure;
+use Balpom\Files\Writer;
+
+abstract class AbstractCreator implements CreatorInterface
+{
+    protected AbstractStructure $structure;
+
+    public function __construct(AbstractStructure $structure)
+    {
+        $this->structure = $structure;
+    }
+
+    abstract public function create(): string;
+
+    /*
+     * Create feed string and write it in file.
+     */
+    public function write(string $filePath): void
+    {
+        $result = $this->create();
+        $writer = new Writer($filePath);
+        $writer->write($result);
+    }
+
+}
---
+++ src/Creators/XmlCreator.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Balpom\Feeder\Creators;
+
+use Balpom\Entity\Structures\AbstractStructure;
+use \DOMDocument as Document;
+use \DOMElement as Element;
+
+abstract class XmlCreator extends AbstractCreator
+{
+    protected Document $xml;
+
+    public function __construct(AbstractStructure $structure)
+    {
+        $this->xml = $this->getDOMDocument();
+        parent::__construct($structure);
+    }
+
+    protected function getDOMDocument(string $version = '1.0', string $encoding = 'UTF-8'): Document
+    {
+        $xml = new Document($version, $encoding);
+        $xml->formatOutput = true;
+
+        return $xml;
+    }
+
+    protected function appendElement(Element &$element, string $name, string $value = ''): void
+    {
+        $newElement = $this->xml->createElement($name, $value);
+        $element->appendChild($newElement);
+    }
+
+    protected function appendNotEmptyElement(Element &$element, string $name, string|null $value = null): void
+    {
+        if (!empty($value)) {
+            $newElement = $this->xml->createElement($name, $value);
+            $element->appendChild($newElement);
+        }
+    }
+
+    protected function appendNotEmptyChild(Element &$element, Element|null $child): void
+    {
+        if (!empty($child)) {
+            $element->appendChild($child);
+        }
+    }
+
+    protected function isEmpty(\Countable|null $countable): bool
+    {
+        if (empty($countable) || 0 === count($countable)) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
